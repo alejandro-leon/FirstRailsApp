d8bb2518c1e63ffd65ccd6037428c982ee8e6293
Alejandro Leon <alex@AlBook.local>
2012-02-29 00:40:23 -0600

    first commit through Aptana Studio 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstRailsApp::Application.config.secret_token = 'b44378b0e9c48c7a0944b25038fb4cd56e79dc8346f4c23b01559db146db6acc6792a80d99cfdcf45f1057dcdaeceb0f6aef553ad7d96de94260024099e88bea'
